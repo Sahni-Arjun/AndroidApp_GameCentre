372495b2b4d17b856d279aae220c607b69977a65
Joon kee Hong <joonkee.hong@mail.utoronto.ca>
2018-11-12 19:56:46 -0500

    Added the isSolvableMethod to Board. Isn't currently implemented in the program.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Board.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Board.java
@@ -97,6 +97,55 @@ public class Board extends Observable implements Serializable, Iterable<Tile> {
     }
 
     /**
+     * Return whether or not the current board is solvable.
+     * The board is said to be solvable if:
+     * 1. If the numRows is odd, then the #inversions is even.
+     * 2. If the numRows is even, and the blank is on an even row, then #inversions is odd.
+     * 3. If the numRows is even, and the blank is on an odd row, then #inversions is even.
+     * The blank row must be counted from the bottom (second-last, fourth-last etc)
+     * <p>
+     * Taken from : https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
+     *
+     * @return whether or not the board is solvable
+     */
+    public boolean isSolvable() {
+        int blankTileId = numRows * numCols;
+        int numInversions = 0;
+        int blankTileRow = 0;
+
+        boolean isSolvable;
+
+        // TODO Maybe find a neater way to iterate through the remaining tiles?
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                Tile currTile = tiles[row][col];
+                // Do not count the inversions on blank tiles
+                if (currTile.getId() != blankTileId) {
+                    // Iterate through each subsequent tiles
+                    for (int subRow = row; subRow < numRows; subRow++) {
+                        for (int subCol = col; subCol < numCols; subCol++) {
+                            Tile subTile = tiles[subRow][subCol];
+                            // Inversions are possible when the nxt tile is less than the curr tile
+                            if (currTile.getId() > subTile.getId()) {
+                                numInversions++;
+                            }
+                        }
+                    }
+                } else {
+                    // Must get the row counting from the bottom
+                    blankTileRow = numRows - row + 1;
+                }
+            }
+        }
+
+        // Formatted version of the condition from above
+        isSolvable = ((numRows % 2 != 0 && numInversions % 2 == 0)) ||
+                (numRows % 2 == 0) && ((blankTileRow % 2 != 0) == (numInversions % 2 == 0));
+
+        return isSolvable;
+    }
+
+    /**
      * Return an iterator for Tiles in this Board.
      *
      * @return an iterator for Tiles
