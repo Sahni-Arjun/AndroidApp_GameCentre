a35831e77c49a95b37f5ea1aa2a93ea7df7f7654
sunyunlee <sunyun.lee@mail.utoronto.ca>
2018-11-30 07:04:17 -0500

    Changes made to SudokuScoreActivityController

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SudokuScoreActivityController.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SudokuScoreActivityController.java
@@ -6,9 +6,20 @@ package fall2018.csc2017.GameCentre;
 import android.content.Context;
 
 public class SudokuScoreActivityController {
+    private FileSystem fileSystem;
+
+    SudokuScoreActivityController(FileSystem fileSystem) {
+        this.fileSystem = fileSystem;
+    }
+
+    /**
+     * Listens for an onCreate call in the activity to return the top scores in Sudoku.
+     *
+     * @param context the current Activity.
+     * @return StringBuilder that will be displayed to the user.
+     */
     public StringBuilder onCreateListener(Context context) {
         Scoreboard sudokuScoreboard;
-        FileSystem fileSystem = new FileSystem();
         sudokuScoreboard = fileSystem.loadScoreboard(context, StartingLoginActivity.SAVE_SUDOKU_SCOREBOARD);
         return sudokuScoreboard.createTopScoreText();
     }
