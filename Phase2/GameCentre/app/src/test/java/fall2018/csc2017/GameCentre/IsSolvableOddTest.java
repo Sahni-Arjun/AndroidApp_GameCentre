7059e3578e1c6f174a835cbc31f8096ce2a44f5a
Joon kee Hong <joonkee.hong@mail.utoronto.ca>
2018-11-30 08:13:22 -0500

    Small changes to IsSolvableTests.

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/IsSolvableOddTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/IsSolvableOddTest.java
@@ -3,10 +3,6 @@ package fall2018.csc2017.GameCentre;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
